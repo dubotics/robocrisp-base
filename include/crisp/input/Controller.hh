2fed14db0a04869239bb7c070cfd59aa2aa2b3f2
Collin J. Sutton <collin.sutton@gmail.com>
2013-11-23 14:22:49 -0800

    Reorganized controller code for cross-platform support
      * Moved platform-specific functionality into EvDevController
    
      * Made Controller a virtual base class.

--- include/crisp/input/Controller.hh
+++ include/crisp/input/Controller.hh
@@ -1,9 +1,8 @@
 #ifndef input_Controller_hh
 #define input_Controller_hh 1
 
-#include <system_error>
 #include <vector>
-#include <unordered_map>
+#include <atomic>
 
 #include <crisp/util/ArrayAccessor.hh>
 #include <crisp/input/Axis.hh>
@@ -16,41 +15,20 @@ namespace input
    */
   class Controller
   {
-  private:
-    int m_fd;
+  protected:
     std::vector<Axis> m_axes;
-    std::unordered_map<uint16_t,size_t> m_axis_map; /**< Mapping from axis code to index. */
-    /* std::vector<Button> m_buttons; */
-    char
-    *m_name,
-      *m_location,
-      *m_identifier;
 
-    ssize_t
-    wait_for_event(struct input_event* e);
-
-  public:		 
-    /** Constructor.
-     *
-     * @param jsdev Joystick device-file path.
-     *
-     * @param evdev `evdev` device file path.
-     */
-    Controller(const char* evdev) throw ( std::system_error );
+  public:
+    Controller();
     virtual ~Controller();
  
     /** Read events from the underlying hardware while a condition flag is set.  This function
      *  blocks execution.
+     *
+     * @param run_flag A flag that will be set to `true` while the controller's
+     *   event-read loop should keep running.
      */
-    void run();
-
-    /** Public, read-only accessors for private state variables.
-     *@{
-     */
-    const char*& name;
-    const char*& location;
-    const char*& identifier;
-    /**@}*/
+    virtual void run(const std::atomic<bool>& run_flag) = 0;
 
     ArrayAccessor<Axis> axes;
   };
