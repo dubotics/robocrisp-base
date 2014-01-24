9be92818e74f516833d4009aa4732ea28c8d6f77
cramertj <cramertaylorj@gmail.com>
2014-01-24 14:56:01 -0800

    Added prototypes to Chassis_Control/ChassisData.h to make it compile-safe for non-Arduino C++ compilers. Changed comment types to .doc comments in ChassisData.h.

--- tests/drive-test/Chassis_Control/ChassisData.h
+++ tests/drive-test/Chassis_Control/ChassisData.h
@@ -9,27 +9,27 @@
 extern "C" {
 #endif
 
-  /* The "packed" attribute prevents GCC from adding extra bytes to the
+  /** The "packed" attribute prevents GCC from adding extra bytes to the
      structure for memory-alignment purposes, which we don't want because the
      structure needs to have the same layout o nall architectures (e.g., your
      computer [probably 64-bit] and the AVR microcontrollers [8-bit]).  */
   typedef struct __attribute__ (( __packed__ ))
   {
-	/*	Whether to speed-control the motors (rather than power control) */
+	/**	Whether to speed-control the motors (rather than power control) */
 	unsigned char pid_enabled : 2;
 
-	/*	Re-initialize the PID loop. This should clear accumulated error when
+	/**	Re-initialize the PID loop. This should clear accumulated error when
 		switched on */
 	unsigned char pid_init : 2;
 
-	/*	Trigger when the arduino should expect new values for P, I, and D from
+	/**	Trigger when the arduino should expect new values for P, I, and D from
 		an external structure. */
 	unsigned char pid_accept_values : 2;
 
-	/*	Left motor speed */
+	/**	Left motor speed */
 	unsigned char left_motor : 5;
 
-	/*	Right motor speed */
+	/**	Right motor speed */
 	unsigned char right_motor : 5;
 
   } ChassisData;
